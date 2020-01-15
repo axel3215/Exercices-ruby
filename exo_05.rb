4ba2ac044b07a97ac343e741c55c7ef9c968e2fc
axel3215 <59627129+axel3215@users.noreply.github.com>
2020-01-15 01:19:52 +0100

    Premier téléchargement exercices 4 à 7

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" # imprime le texte
+puts "Travail : #{10 * 5 * 11}" # imprime le texte et afficher le résultat de l'équation
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #imprimer le texte et afficher le résultat de l'équation
+
+puts "Et en secondes ?" # imprime le texte
+
+puts 10 * 5 * 11 * 60 * 60 # imprime le texte et affiche le résultat de l'équation
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" # imprime l'équation et affiche le résultat de l'équation
+
+puts 3 + 2 < 5 - 7 # imprime le texte 
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # imprime le texte et afficher le résultat de l'équation
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # imprime le texte et afficher le résultat de l'équation
+
+puts "Ok, c'est faux alors !" # imprime le texte
+
+puts "C'est drôle ça, faisons-en plus :" # imprime le texte
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # imprime le texte et affiche la réponse à la question
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" # imprime le texte et afficher la réponse à la question
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" # imprime le texte et afficher la réponse à la question
